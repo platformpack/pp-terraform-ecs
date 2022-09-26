245a1ec19443c0bc34911df0f7ac49a95a41ab96
Gheorghe Casian <Gheorghe_Casian@epam.com>
2022-09-26 14:59:26 +0300

    Adds SonarQube integration

---
+++ variables.tf
@@ -0,0 +1,18 @@
+locals {
+  region = "eu-west-1"
+  name   = "ecs-ex-${replace(basename(path.cwd), "_", "-")}"
+
+  user_data = <<-EOT
+    #!/bin/bash
+    cat <<'EOF' >> /etc/ecs/ecs.config
+    ECS_CLUSTER=${local.name}
+    ECS_LOGLEVEL=debug
+    EOF
+  EOT
+
+  tags = {
+    Name       = local.name
+    Example    = local.name
+    Repository = "https://github.com/terraform-aws-modules/terraform-aws-ecs"
+  }
+}
