dfe78776bd6f624f10da8950f1dc7a23b1660e37
Gheorghe Casian <Gheorghe_Casian@epam.com>
2022-10-05 23:07:39 +0300

    Testing

--- dirtycode.tf
+++ dirtycode.tf
@@ -24,3 +24,18 @@ resource "aws_ec2_host" "test" {
   host_recovery     = "on"
   auto_placement    = "on"
 }
+
+
+resource "aws_efs_file_system" "sharedstore" {
+  creation_token = var.efs["creation_token"]
+
+  lifecycle_policy {
+    transition_to_ia = var.efs["transition_to_ia"]
+  }
+
+  kms_key_id                      = var.efs["kms_key_id"]
+  encrypted                       = false
+  performance_mode                = var.efs["performance_mode"]
+  provisioned_throughput_in_mibps = var.efs["provisioned_throughput_in_mibps"]
+  throughput_mode                 = var.efs["throughput_mode"]
+}
