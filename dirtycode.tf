e49c4b4df3eb4be85cbfbfd96eea2f985fd71667
Gheorghe Casian <Gheorghe_Casian@epam.com>
2022-10-05 22:45:54 +0300

    More Testing

--- dirtycode.tf
+++ dirtycode.tf
@@ -6,3 +6,12 @@ resource "aws_security_group_rule" "allow_all" {
   from_port         = 0
   security_group_id = "sg-123456"
 }
+
+resource "aws_security_group_rule" "example" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = "0.0.0.0/0"
+  security_group_id = "sg-123456"
+}
